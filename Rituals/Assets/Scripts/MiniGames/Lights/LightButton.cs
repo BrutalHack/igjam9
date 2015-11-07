0054c40b7ef6f801360fbecce25ebd4c46d9847f
Eike Wulff <e.wulff@aleron.eu>
2015-11-07 13:40:13 +0100

    made light game

---
+++ Rituals/Assets/Scripts/MiniGames/Lights/LightButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MiniGames.Lights
+{
+	public class LightButton : MonoBehaviour
+	{
+		public LightsLayout layout;
+		public Position2d position;
+
+		void OnMouseDown ()
+		{
+			layout.click (position);
+		}
+	}
+}
