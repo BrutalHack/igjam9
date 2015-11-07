df1b8b4bba403c7d9ca1049d78ca494b30b2bf18
Benjamin Justice <b.justice@aleron.eu>
2015-11-07 12:58:02 +0100

    Face skincolor changing

--- Rituals/Assets/Scripts/MiniGames/Faces/Face.cs
+++ Rituals/Assets/Scripts/MiniGames/Faces/Face.cs
@@ -5,24 +5,33 @@ namespace MiniGames.Faces
 {
 	public class Face : MonoBehaviour
 	{
-		public Sprite FaceSprite;
 		public Sprite EyeSprite;
 		public Sprite[] HairSprites;
 		public Sprite[] EyebrowSprites;
 		public Sprite[] NoseSprites;
 		public Sprite[] MouthSprites;
 		public Color SkinColor;
+		public SpriteRenderer head;
+		public SpriteRenderer hair;
+		public SpriteRenderer eyebrow;
+		public SpriteRenderer nose;
+		public SpriteRenderer mouth;
 
 		// Use this for initialization
 		void Start ()
 		{
-	
+			
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
 	
+		}
+
+		void OnValidate ()
+		{
+			head.color = SkinColor;
 		}
 	}
 }
