12631af9a5969556de4eaf59123ba24c68c99c2f
Sergej Tihonov <s.tihonov@aleron.eu>
2015-11-07 13:10:37 +0100

    Justice stört mich wieder beim Arbeiten.

---
+++ Rituals/Assets/Scripts/DragHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+namespace MainGame
+{
+
+	public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+	{
+
+		private Vector3 startPosition;
+
+		public void OnBeginDrag (PointerEventData eventData)
+		{
+			startPosition = transform.position;
+		}
+
+		public void OnDrag (PointerEventData eventData)
+		{
+			throw new System.NotImplementedException ();
+		}
+
+		public void OnEndDrag (PointerEventData eventData)
+		{
+			throw new System.NotImplementedException ();
+		}
+	}
+}
