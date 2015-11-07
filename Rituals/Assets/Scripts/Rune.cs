76bea04340b84222e83211e20579d0388c48e7a1
Sergej Tihonov <s.tihonov@aleron.eu>
2015-11-07 12:30:01 +0100

    Generierung von Runen.

---
+++ Rituals/Assets/Scripts/Rune.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace MainGame
+{
+
+	public class Rune : MonoBehaviour
+	{
+
+		public Image SymbolImage;
+		public Transform StartPosition;
+
+		public void ResetPosition ()
+		{
+			this.gameObject.transform.position = StartPosition.position;
+		}
+	}
+}
---
+++ Rituals/Assets/Scripts/RuneManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MainGame
+{
+
+	public class RuneManager : MonoBehaviour
+	{
+
+		public Transform[] RunePositions = new Transform[8];
+		public GameObject RunePrefab;
+		private List<Rune> runes = new List<Rune> ();
+
+		void Start ()
+		{
+	
+		}
+
+		void Update ()
+		{
+	
+		}
+
+		public void GenerateOrUpdateRunes (Symbol[] symbols)
+		{
+			while (runes.Count > 0) {
+				Destroy (runes [0].gameObject);
+				runes.RemoveAt (0);
+			}
+			for (int i = 0; i < symbols.Length; i++) {
+				Rune rune = Instantiate (RunePrefab).gameObject.GetComponent <Rune> ();
+				rune.gameObject.transform.SetParent (this.gameObject.transform, false);
+				rune.StartPosition = RunePositions [i];
+				rune.ResetPosition ();
+				rune.SymbolImage.sprite = symbols [i].Sprite;
+				runes.Add (rune);
+			}
+		}
+	}
+}
