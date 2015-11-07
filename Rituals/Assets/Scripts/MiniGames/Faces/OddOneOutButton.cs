9af360452bb3890c1db8fa3ea90de4027e9dea13
Benjamin Justice <b.justice@aleron.eu>
2015-11-07 16:47:32 +0100

    Moved all Sprites into one folder; finished OddOneOut game

---
+++ Rituals/Assets/Scripts/MiniGames/Faces/OddOneOutButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OddOneOutButton : MonoBehaviour
+{
+	public bool correctFace;
+
+	void OnMouseDown ()
+	{
+		if (correctFace) {
+			Debug.Log ("Correct Face! You won!");
+		} else {
+			Debug.Log ("Wrong Face! You lost!");
+		}
+	}
+}
