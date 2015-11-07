384294130d6681386b77571b0b40d985564e231a
Benjamin Justice <b.justice@aleron.eu>
2015-11-07 20:24:54 +0100

    Music Manager

---
+++ Rituals/Assets/MusicManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MainGame
+{
+
+	[RequireComponent (typeof(AudioSource))]
+	public class MusicManager : MonoBehaviour
+	{
+		[SerializeField]
+		private AudioClip[] PlayList;
+		[SerializeField]
+		private int nowPlaying;
+		private AudioSource audioSource;
+
+		void Awake ()
+		{
+			DontDestroyOnLoad (this.gameObject);
+			audioSource = GetComponent <AudioSource> ();
+			audioSource.loop = false;
+		}
+
+		// Use this for initialization
+		void Start ()
+		{
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+			if (!audioSource.isPlaying || Input.GetKeyDown (KeyCode.Space)) {
+				nowPlaying++;	
+				if (nowPlaying >= PlayList.Length) {
+					nowPlaying = 0;
+				}
+				PlayTrack ();
+			}
+		}
+
+		void PlayTrack ()
+		{
+			audioSource.clip = PlayList [nowPlaying];
+			audioSource.Play ();
+		}
+	}
+}
