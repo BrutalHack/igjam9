f53e3a6c93255d20dbb54172f2b48dd280051a23
Benjamin Justice <b.justice@aleron.eu>
2015-11-07 13:11:19 +0100

    Face prefab, another unity bug strkes

--- Rituals/Assets/Scripts/MiniGames/Faces/Face.cs
+++ Rituals/Assets/Scripts/MiniGames/Faces/Face.cs
@@ -5,17 +5,38 @@ namespace MiniGames.Faces
 {
 	public class Face : MonoBehaviour
 	{
-		public Sprite EyeSprite;
-		public Sprite[] HairSprites;
-		public Sprite[] EyebrowSprites;
-		public Sprite[] NoseSprites;
-		public Sprite[] MouthSprites;
-		public Color SkinColor;
-		public SpriteRenderer head;
-		public SpriteRenderer hair;
-		public SpriteRenderer eyebrow;
-		public SpriteRenderer nose;
-		public SpriteRenderer mouth;
+		public int ActiveSkinColor;
+		public int ActiveHairColor;
+		public int ActiveHair;
+		public int ActiveEyebrows;
+		public int ActiveNose;
+		public int ActiveMouth;
+
+		[SerializeField]
+		private Sprite EyeSprite;
+		[SerializeField]
+		private Sprite[] HairSprites;
+		[SerializeField]
+		private Sprite[] EyebrowSprites;
+		[SerializeField]
+		private Sprite[] NoseSprites;
+		[SerializeField]
+		private Sprite[] MouthSprites;
+		[SerializeField]
+		private Color[] SkinColors;
+		[SerializeField]
+		private Color[] HairColors;
+		[SerializeField]
+		private SpriteRenderer head;
+		[SerializeField]
+		private SpriteRenderer hair;
+		[SerializeField]
+		private SpriteRenderer eyebrow;
+		[SerializeField]
+		private SpriteRenderer nose;
+		[SerializeField]
+		private SpriteRenderer mouth;
+		private Color NoseColorDiff = new Color (30, 30, 30);
 
 		// Use this for initialization
 		void Start ()
@@ -31,7 +52,8 @@ namespace MiniGames.Faces
 
 		void OnValidate ()
 		{
-			head.color = SkinColor;
+			head.color = SkinColors [ActiveSkinColor];
+			nose.color = SkinColors [ActiveHairColor] - NoseColorDiff;
 		}
 	}
 }
