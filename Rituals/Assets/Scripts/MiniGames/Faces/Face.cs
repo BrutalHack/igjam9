29f1d24151ac66b32958f87f8ad2688d2d2f25d0
Benjamin Justice <b.justice@aleron.eu>
2015-11-07 12:30:59 +0100

    Face stuff

--- Rituals/Assets/Scripts/MiniGames/Faces/Face.cs
+++ Rituals/Assets/Scripts/MiniGames/Faces/Face.cs
@@ -5,6 +5,14 @@ namespace MiniGames.Faces
 {
 	public class Face : MonoBehaviour
 	{
+		public Sprite FaceSprite;
+		public Sprite EyeSprite;
+		public Sprite[] HairSprites;
+		public Sprite[] EyebrowSprites;
+		public Sprite[] NoseSprites;
+		public Sprite[] MouthSprites;
+		public Color SkinColor;
+
 		// Use this for initialization
 		void Start ()
 		{
