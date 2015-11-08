e434590972343156233c0c261cbf1de2c9ab9011
Benjamin Justice <b.justice@aleron.eu>
2015-11-08 09:30:57 +0100

    Pulsating UI buttons; text instructions for each puzzle; readability

---
+++ Rituals/Assets/Scripts/Pulsate.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+namespace MainGame
+{
+	
+	public class Pulsate : MonoBehaviour
+	{
+		public float speed;
+		public float pulsateSize;
+
+		SpriteRenderer spriteRenderer;
+		Vector3 baseScale;
+
+		// Use this for initialization
+		void Start ()
+		{
+			spriteRenderer = GetComponent<SpriteRenderer> ();
+			baseScale = transform.localScale;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+			transform.localScale = baseScale +
+			(Vector3.one * pulsateSize * (float)Math.Sin (Time.timeSinceLevelLoad * speed));
+		}
+	}
+}
