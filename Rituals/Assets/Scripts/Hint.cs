7b6b28d01a390520c6d6e5b88ef3b64e9285b52c
Sergej Tihonov <s.tihonov@aleron.eu>
2015-11-08 03:14:51 +0100

    line renderer eingeabut probleme mit dem alten Drag and Drop und das loeschen der Lines funktioniert nicht.

--- Rituals/Assets/Scripts/Hint.cs
+++ Rituals/Assets/Scripts/Hint.cs
@@ -2,19 +2,23 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class Hint
+namespace MainGame
 {
-	
-	public readonly Sprite SymbolASprite;
-	public readonly Sprite RelationshipSprite;
-	public readonly Sprite SymbolBSprite;
-	public readonly int MinigameScene;
 
-	public Hint (Sprite SymbolASprite, Sprite RelationshipSprite, Sprite SymbolBSprite, int MinigameScene)
+	public class Hint
 	{
-		this.SymbolASprite = SymbolASprite;
-		this.RelationshipSprite = RelationshipSprite;
-		this.SymbolBSprite = SymbolBSprite;
-		this.MinigameScene = MinigameScene;
+	
+		public readonly Sprite SymbolASprite;
+		public readonly Sprite RelationshipSprite;
+		public readonly Sprite SymbolBSprite;
+		public readonly int MinigameScene;
+
+		public Hint (Sprite SymbolASprite, Sprite RelationshipSprite, Sprite SymbolBSprite, int MinigameScene)
+		{
+			this.SymbolASprite = SymbolASprite;
+			this.RelationshipSprite = RelationshipSprite;
+			this.SymbolBSprite = SymbolBSprite;
+			this.MinigameScene = MinigameScene;
+		}
 	}
 }
